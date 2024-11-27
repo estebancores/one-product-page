dbeb947016003b1b304857c67c3c81e628892f54
unknown <estebancores@gmail.com>
2024-11-26 22:48:28 -0500

    initial commit

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics, logEvent } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXygxz8ZbsNuNy9L3ugoCEFlKy383Osd8",
-  authDomain: "planner-selling.firebaseapp.com",
-  projectId: "planner-selling",
-  storageBucket: "planner-selling.firebasestorage.app",
-  messagingSenderId: "871174889145",
-  appId: "1:871174889145:web:4c26fdad12c0839c4c8568",
-  measurementId: "G-X28819WWXE"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
